cd353918043096dfd6ada84a37e2c04bb4b3689d
Alex Graham <destgraham@icloud.com>
2024-06-12 18:51:37 -0700

    new: "ct-utils style classes"

---
+++ .changeset/polite-jobs-impress.md
@@ -0,0 +1,5 @@
+---
+"@ag108/ct-symbols_libs": patch
+---
+
+added [symbols] pack ct-utils stylesheets
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { TabDispatchAction } from '@ag108/ct-utils/types/dispatch';
 import { PackageLibraries } from '@ag108/ct-utils/types/main';
 
-import { PackageIndexLink } from '@ag108/ct-utils';
+import { cn, PackageIndexLink } from '@ag108/ct-utils';
 
 import { FacesState, FlagsState, FoodAndBevState, HeartsState, MoreSymbolsState, NatureState, ObjectsState } from '~/state';
 
@@ -13,25 +13,25 @@ const libraries: PackageLibraries = [
 	{ name: 'hearts', tabState: HeartsState },
 	{ name: 'flags', tabState: FlagsState },
 	{ name: 'more...', tabState: MoreSymbolsState },
-]
-		
-export const IndexEmote = ({setTab}: {setTab: TabDispatchAction}) => {
+];
+
+export const IndexEmote = ({ setTab }: { setTab: TabDispatchAction }) => {
 	return (
-		<article className='flex flex-col pt-2'>
-			<p className='text-accent uppercase'>
+		<article className={cn('ct-utils_flex', 'ct-utils_flex-column', 'ct-utils_pad-top-2')}>
+			<p className={cn('ct-utils_text-accent', 'ct-utils_uppercase')}>
 				[<PackageIndexLink setTab={setTab} />] symbols & emoticons ({libraries.length} items):
 			</p>
-			<ol className='flex flex-col px-4 items-start h-[140px] overflow-scroll'>
+			<ol className={cn('ct-utils_flex', 'ct-utils_flex-column', 'ct-utils_pad-x-4', 'ct-utils_items-start', 'ct-utils_h-15rem', 'ct-utils_overflow-scroll')}>
 				{libraries.map((library, i) => (
 					<li key={i + 1}>
-						<button onClick={() => setTab(library.tabState)} className='uppercase outline-none'>
-							{i + 1}) [<span className='hover:underline cursor-pointer'>{library.name}</span>]
+						<button onClick={() => setTab(library.tabState)} className={cn('ct-utils_uppercase', 'ct-utils_outline-none')}>
+							{i + 1}) [<span className={cn('ct-utils_hover underline', 'ct-utils_cursor-pointer')}>{library.name}</span>]
 						</button>
 					</li>
 				))}
 			</ol>
 		</article>
 	);
-}
+};
 
-export { Faces, FoodAndBev, Nature, Objects, Hearts, Flags, MoreText } from '~/libs'
+export { Faces, FoodAndBev, Nature, Objects, Hearts, Flags, MoreText } from '~/libs';
